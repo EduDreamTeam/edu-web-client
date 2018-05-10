4b06390e0c1ba2ec47eeb13bdff993e07e70f205
beltasha <nataly9682@gmail.com>
2018-05-10 23:27:59 +0300

    remove old incorrect tests

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,43 +1,44 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
 import {
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    MatRadioModule,
-  } from '@angular/material';
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatRadioModule,
+} from "@angular/material";
 import {
-    FormsModule,
-    ReactiveFormsModule,
+  FormsModule,
+  ReactiveFormsModule,
 } from "@angular/forms";
-import { FlexLayoutModule } from '@angular/flex-layout';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 const FORMS_MODULES = [
-    FormsModule,
-    ReactiveFormsModule,
-]
+  FormsModule,
+  ReactiveFormsModule,
+];
 
 const MATERIAL_MODULES = [
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    MatRadioModule,
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatRadioModule,
 ];
 
 @NgModule({
-    exports: [
-        CommonModule,
-        ...FORMS_MODULES,
-        FlexLayoutModule,
-        ...MATERIAL_MODULES,
-    ],
-    imports: [
-        CommonModule,
-        ...FORMS_MODULES,
-        FlexLayoutModule,
-        ...MATERIAL_MODULES,
-    ],
+  exports: [
+    CommonModule,
+    ...FORMS_MODULES,
+    FlexLayoutModule,
+    ...MATERIAL_MODULES,
+  ],
+  imports: [
+    CommonModule,
+    ...FORMS_MODULES,
+    FlexLayoutModule,
+    ...MATERIAL_MODULES,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {
+}
