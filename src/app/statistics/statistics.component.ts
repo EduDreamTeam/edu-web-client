f4c379de8c9faf0fc503cbb45f7a71a24a768b46
beltasha <nataly9682@gmail.com>
2018-05-18 13:18:22 +0300

    implement statistics tests

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from "@angular/core";
 
 @Component({
-  selector: 'app-statistics',
-  templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.css']
+  selector: "app-statistics",
+  templateUrl: "./statistics.component.html",
+  styleUrls: ["./statistics.component.css"]
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent {
+  @Input()
+  public result: any[];
 
-  constructor() { }
 
-  ngOnInit() {
+  public get labels() {
+    return this.result.map(res => res.date.toISOString());
+  }
+
+  public get data() {
+    return [{
+      data: this.result.map(res => res.result * 100),
+      label: "Результаты тестирования"
+    }];
   }
 
 }
