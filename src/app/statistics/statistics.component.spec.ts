b897ecec5fdd8794a0c57b39b14844aae20d950a
beltasha <nataly9682@gmail.com>
2018-05-29 01:16:18 +0300

    implement bdd in tests

--- src/app/statistics/statistics.component.spec.ts
+++ src/app/statistics/statistics.component.spec.ts
@@ -23,6 +23,114 @@ const FAKE_RESULT = [
   }
 ];
 
+const givenCorrectlyConfigured = (component) => {
+  component.filter.startDate = new Date(2018, 3, 3);
+  component.filter.endDate = new Date(2018, 4, 3);
+  component.filter.minResult = 40;
+  component.filter.maxResult = 50;
+}
+
+const givenTooBigMaxResultConfigured = (component) => {
+  component.filter.startDate = new Date(2000, 0, 1);
+  component.filter.endDate = new Date(2019, 0, 1);
+  component.filter.minResult = 0;
+  component.filter.maxResult = 101;
+}
+
+const givenTooSmallMaxResultConfigured = (component) => {
+  component.filter.startDate = new Date(2000, 0, 1);
+  component.filter.endDate = new Date(2019, 0, 1);
+  component.filter.minResult = 0;
+  component.filter.maxResult = -4;
+}
+
+const givenNotIntMaxResultConfigured = (component) => {
+  component.filter.startDate = new Date(2000, 0, 1);
+  component.filter.endDate = new Date(2019, 0, 1);
+  component.filter.minResult = 0;
+  component.filter.maxResult = "bad";
+}
+
+const givenTooBigMinResultConfigured = (component) => {
+  component.filter.startDate = new Date(2000, 0, 1);
+  component.filter.endDate = new Date(2019, 0, 1);
+  component.filter.minResult = 101;
+  component.filter.maxResult = 50;
+}
+
+const givenTooSmallMinResultConfigured = (component) => {
+  component.filter.startDate = new Date(2000, 0, 1);
+  component.filter.endDate = new Date(2019, 0, 1);
+  component.filter.minResult = -3;
+  component.filter.maxResult = -19;
+}
+
+const givenNotIntMinResultConfigured = (component) => {
+  component.filter.startDate = new Date(2000, 0, 1);
+  component.filter.endDate = new Date(2019, 0, 1);
+  component.filter.minResult = "bad";
+  component.filter.maxResult = 50;
+}
+
+const givenStartBiggerThanEndConfigured = (component) => {
+  component.filter.startDate = new Date(2019, 0, 1);
+  component.filter.endDate = new Date(2000, 0, 1);
+  component.filter.minResult = 1;
+  component.filter.maxResult = 50;
+}
+
+const givenNotIntEndDateResultConfigured = (component) => {
+  component.filter.startDate = new Date(2000, 0, 1);
+  component.filter.endDate = "bad";
+  component.filter.minResult = 0;
+  component.filter.maxResult = 50;
+}
+
+const givenNotIntStartDateResultConfigured = (component) => {
+  component.filter.startDate = "bad";
+  component.filter.endDate =  new Date(2000, 0, 1);
+  component.filter.minResult = 0;
+  component.filter.maxResult = 50;
+}
+
+const whenSearchIsInvoked = (component) => {
+  spyOn(component.sendFilter, "emit");
+  component.onSendFilter();
+}
+
+const thenSearchRequestSentToServer = (component) => {
+  expect(component.sendFilter.emit).toHaveBeenCalledWith({
+    startDate: new Date(2018, 3, 3),
+    endDate: new Date(2018, 4, 3),
+    minResult: 0.4,
+    maxResult: 0.5
+  });
+}
+
+const thenErrorMessageMaxResultCalculate =(component) => {
+  expect(component.errorMessage).toEqual("Введите корректный максимальный результат");
+}
+
+const thenErrorMessageMinResultCalculate =(component) => {
+  expect(component.errorMessage).toEqual("Введите корректный минимальный результат");
+}
+
+const thenErrorMessageStartEndDateCalculate = (component) => {
+  expect(component.errorMessage).toEqual("Дата начала не может быть больше даты конца");
+}
+
+const thenErrorMessageEndDateCalculate = (component) => {
+  expect(component.errorMessage).toEqual("Введите корректную дату конца");
+}
+
+const thenErrorMessageStartDateCalculate = (component) => {
+  expect(component.errorMessage).toEqual("Введите корректную дату начала");
+}
+
+const thenErrorMessageEmptyCalculate = (component) => {
+  expect(component.errorMessage).toEqual("");
+}
+
 describe("StatisticsComponent", () => {
   let component: StatisticsComponent;
   let fixture: ComponentFixture<StatisticsComponent>;
@@ -52,9 +160,9 @@ describe("StatisticsComponent", () => {
   describe("Getters", () => {
     it("should return array of dates as lables", () => {
       const FAKE_LABELS = [
-        (new Date(2018, 2, 20)).toISOString(),
-        (new Date(2018, 3, 10)).toISOString(),
-        (new Date(2018, 4, 1)).toISOString()
+        "20-2-2018",
+        "10-3-2018",
+        "1-4-2018"
       ];
       expect(component.labels).toEqual(FAKE_LABELS);
     });
@@ -67,136 +175,61 @@ describe("StatisticsComponent", () => {
     });
 
     it("ErrorMessage: should return empty string", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2000, 0, 1),
-        endDate: new Date(2019, 0, 1),
-        minResult: 0,
-        maxResult: 100
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("");
+      givenCorrectlyConfigured(component);
+      thenErrorMessageEmptyCalculate(component);
     });
 
     it("ErrorMessage: should return 'Введите корректную дату начала'", () => {
-      const CORRECT_FILTER = {
-        startDate: "bad date",
-        endDate: new Date(2019, 0, 1),
-        minResult: 0,
-        maxResult: 100
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректную дату начала");
+      givenNotIntStartDateResultConfigured(component)
+      thenErrorMessageStartDateCalculate(component)
     });
 
     it("ErrorMessage: should return 'Введите корректную дату конца'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2019, 0, 1),
-        endDate: "bad date",
-        minResult: 0,
-        maxResult: 100
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректную дату конца");
+      givenNotIntEndDateResultConfigured(component);
+      thenErrorMessageEndDateCalculate(component);
     });
 
     it("ErrorMessage: should return 'Дата начала не может быть больше даты конца'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2019, 0, 1),
-        endDate: new Date(2000, 0, 1),
-        minResult: 0,
-        maxResult: 100
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Дата начала не может быть больше даты конца");
+      givenStartBiggerThanEndConfigured(component);
+      thenErrorMessageStartEndDateCalculate(component);
     });
 
     it("ErrorMessage: should return 'Введите корректный минимальный результат'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2000, 0, 1),
-        endDate: new Date(2019, 0, 1),
-        minResult: "bad",
-        maxResult: 100
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректный минимальный результат");
+      givenNotIntMinResultConfigured(component);
+      thenErrorMessageMinResultCalculate(component);
     });
 
     it("ErrorMessage: should return 'Введите корректный минимальный результат'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2000, 0, 1),
-        endDate: new Date(2019, 0, 1),
-        minResult: -1,
-        maxResult: 100
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректный минимальный результат");
+      givenTooSmallMinResultConfigured(component);
+      thenErrorMessageMinResultCalculate(component);
     });
 
     it("ErrorMessage: should return 'Введите корректный минимальный результат'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2000, 0, 1),
-        endDate: new Date(2019, 0, 1),
-        minResult: 101,
-        maxResult: 100
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректный минимальный результат");
+      givenTooBigMinResultConfigured(component);
+      thenErrorMessageMinResultCalculate(component);
     });
 
     it("ErrorMessage: should return 'Введите корректный максимальный результат'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2000, 0, 1),
-        endDate: new Date(2019, 0, 1),
-        minResult: 0,
-        maxResult: "bad"
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректный максимальный результат");
+      givenNotIntMaxResultConfigured(component);
+      thenErrorMessageMaxResultCalculate(component);
     });
 
     it("ErrorMessage: should return 'Введите корректный максимальный результат'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2000, 0, 1),
-        endDate: new Date(2019, 0, 1),
-        minResult: 0,
-        maxResult: -9
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректный максимальный результат");
+      givenTooSmallMaxResultConfigured(component);
+      thenErrorMessageMaxResultCalculate(component);
     });
 
     it("ErrorMessage: should return 'Введите корректный максимальный результат'", () => {
-      const CORRECT_FILTER = {
-        startDate: new Date(2000, 0, 1),
-        endDate: new Date(2019, 0, 1),
-        minResult: 0,
-        maxResult: 101
-      };
-      component.filter = CORRECT_FILTER;
-      expect(component.errorMessage).toEqual("Введите корректный максимальный результат");
+      givenTooBigMaxResultConfigured(component);
+      thenErrorMessageMaxResultCalculate(component);
     });
   });
 
   describe("Methods", () => {
     it("should action with current filters", () => {
-
-      givenProperlyConfigured();
-      component.filter.startDate = new Date(2018, 3, 3);
-      component.filter.endDate = new Date(2018, 4, 3);
-      component.filter.minResult = 40;
-      component.filter.maxResult = 50;
-
-      whenSearchIsInvoked();
-      spyOn(component.sendFilter, "emit");
-      component.onSendFilter();
-
-      thenProperSearchRequestSentToServer();
-      expect(component.sendFilter.emit).toHaveBeenCalledWith({
-        startDate: new Date(2018, 3, 3),
-        endDate: new Date(2018, 4, 3),
-        minResult: 0.2,
-        maxResult: 0.5
-      });
+      givenCorrectlyConfigured(component);
+      whenSearchIsInvoked(component);
+      thenSearchRequestSentToServer(component);
     });
   });
 
